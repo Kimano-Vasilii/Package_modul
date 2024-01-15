8fa0a6243531a93bd6a73e32a1457477b19f1740
Maksin Vasilii <Kimano.arb@mail.ru>
2024-01-15 18:38:25 +0500

    add file

---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+if __name__ == '__main__':
+    current_date = datetime.date.today()
+    print(f"Текущая дата: {current_date}")
+    calculate_salary()
+    get_employees()
